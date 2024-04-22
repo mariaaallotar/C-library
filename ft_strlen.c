5d20dcbf69298fb11de226c86e8c1462a84bf92d
Maria Helenius <maheleni@c2r4p5.hive.fi>
2024-04-22 14:27:14 +0300

    Fixed norm errors

--- ft_strlen.c
+++ ft_strlen.c
@@ -21,7 +21,7 @@
 */
 size_t	ft_strlen(const char *s)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (*s != '\0')
--- tests.h
+++ tests.h
@@ -18,15 +18,15 @@
 # include <xlocale.h>
 # include <strings.h>
 
-int	ft_islower(int c);
-int	ft_isupper(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_islower(int c);
+int		ft_isupper(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t len);
