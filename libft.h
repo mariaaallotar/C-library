727c5fabeaaeeb2bd8c59239ef785decb7aae001
Maria Helenius <maheleni@c2r3p5.hive.fi>
2024-04-25 11:23:11 +0300

    Fixed norm errors

--- libft.h
+++ libft.h
@@ -16,13 +16,13 @@
 # include <stdio.h>
 
 void	ft_bzero(void *s, size_t n);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_islower(int c);
-int	ft_isprint(int c);
-int	ft_isupper(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_islower(int c);
+int		ft_isprint(int c);
+int		ft_isupper(int c);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t len);
@@ -31,8 +31,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 char	*ft_strrchr(const char *s, int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
