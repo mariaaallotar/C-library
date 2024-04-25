6d795e5eace8fe37d35a34c1d4aae743612bd3ca
Maria Helenius <maheleni@c2r3p5.hive.fi>
2024-04-25 09:20:31 +0300

    Ft_memcpy now returns NULL with both src and dst NULL pointers

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -27,8 +27,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	void	*orig_dst;
 
 	orig_dst = dst;
-	if ((void *) src == (void *) 0)
-		return (orig_dst);
+	if (src == NULL && dst == NULL)
+		return (NULL);
 	while (n > 0)
 	{
 		*(char *)dst = *(char *)src;
