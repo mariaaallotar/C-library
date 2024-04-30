b8e9c05e8bc6db4f968a36e39db2e34e058b0fae
Maria Helenius <maheleni@c2r3p7.hive.fi>
2024-04-30 10:29:19 +0300

    ft-memchr now does NOT stop seaching after '\0', added comments

--- ft_memchr.c
+++ ft_memchr.c
@@ -12,16 +12,28 @@
 
 #include "libft.h"
 
+/**
+* Locates the first occurrence of c (converted to an unsigned char) in s.
+* Continues searching even after '\0' if there is still n left.
+* Parameters:
+*	*s - pointer to memory to search from
+*	c - int representation of character to find
+*	n - amount of memorylocations to search
+* Returns:
+*	Location (void pinter) of first occurrance where c was found
+*	NULL if c was NOT found anywhere
+*/
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	while ((unsigned char) *(char *) s != '\0' && n > 0)
+	unsigned char	c_n_str;
+
+	while (n > 0)
 	{
-		if ((unsigned char) *(char *) s == (unsigned char) c)
+		c_n_str = (unsigned char) *(char *) s;
+		if (c_n_str == (unsigned char) c)
 			return ((void *) s);
 		s++;
 		n--;
 	}
-	if ((unsigned char) *(char *) s != '\0' && c == '\0' && n > 0)
-		return ((void *) s);
 	return (NULL);
 }
