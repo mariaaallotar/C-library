a038f2b7b2f9dd679fb05bc8e8ef13848f0f675b
Maria Helenius <maheleni@c2r3p5.hive.fi>
2024-05-01 18:37:08 +0300

    ft_itoa now working + added comments

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,30 +12,73 @@
 
 #include "libft.h"
 
+/**
+* Counts how many digits are in the number n
+* Parameters:
+*	n - number to count digits of
+*/
+static int	count_digits(long n)
+{
+	int	count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	while (n != 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
+}
+
+/**
+* Converts an integer to ascii into the string given
+* Parameters:
+*	num - number to convert
+*	*str - string to put the number into
+*	i - last index of string
+*/
+static void	convert_integer(long num, char *str, int i)
+{
+	while (i-- > 0)
+	{
+		str[i] = (num % 10) + '0';
+		num = num / 10;
+	}
+}
+
+/**
+* Allocates and returns a string representing the integer received as an
+* argument
+* Parameters:
+*	n - int number
+* Returns:
+*	A string of that number
+*/
 char	*ft_itoa(int n)
 {
-	int		magnitude;
-	int		magnitude_c;
+	int		is_neg;
+	int		digits;
 	char	*a;
-	char	*orig_a;
+	int		i;
+	long	num;
 
-	magnitude = 1;
-	magnitude_c = 1;
-	while (n / (magnitude * 10) >= 1)
+	num = (long) n;
+	is_neg = 0;
+	if (num < 0)
 	{
-		magnitude = magnitude * 10;
-		magnitude_c++;
+		is_neg = 1;
+		num = -num;
 	}
-	a = (char *) malloc(magnitude_c * sizeof(char));
+	digits = count_digits(num);
+	i = digits + is_neg;
+	a = (char *)malloc(sizeof(char) * (i + 1));
 	if (a == NULL)
 		return (NULL);
-	orig_a = a;
-	while (n >= 0)
-	{
-		*a = (n / magnitude) + '0';
-		n = n - (magnitude * (n / magnitude));
-		magnitude = magnitude / 10;
-		a++;
-	}
-	return (orig_a);
+	a[i] = '\0';
+	convert_integer(num, a, i);
+	if (is_neg)
+		a[0] = '-';
+	return (a);
 }
